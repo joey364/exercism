f66e09774b95d7d243f20bd63856e4708dba3918
joey364 <71462893+joey364@users.noreply.github.com>
2022-04-03 00:31:19 +0000

    armstrong_nums(bash): complete!

---
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+main() {
+	# determine whether a number is an Armstrong number.
+	local num=$1
+	local sum=0
+	local temp=$num
+	local digit_count=${#num}
+
+	while [ $temp -gt 0 ]; do
+		local remainder=$(($temp % 10))
+		sum=$(($sum + $remainder ** $digit_count))
+		temp=$(($temp / 10))
+	done
+
+	if [ $sum -eq $num ]; then
+		echo "true"
+	else
+		echo "false"
+	fi
+}
+
+main "$@"
