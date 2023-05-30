2ca1cd2f3fec004ecb9226fe22f4174d8d9349a7
joey36 <71462893+joey364@users.noreply.github.com>
2023-05-30 15:46:04 +0000

    csharp(weighing-machine): complete!

---
+++ csharp/weighing-machine/WeighingMachine.cs
@@ -0,0 +1,37 @@
+using System;
+
+class WeighingMachine
+{
+    private double weight = 0;
+
+    public WeighingMachine(int precision)
+    {
+        Precision = precision;
+    }
+
+    public int Precision { get; }
+
+    public double Weight
+    {
+        get
+        {
+            return weight;
+        }
+        set
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException();
+            weight = value;
+        }
+    }
+
+    public double TareAdjustment { get; set; } = 5.0;
+
+
+    public string DisplayWeight
+    {
+        get
+        {
+            return $"{Math.Round(Weight - TareAdjustment, Precision).ToString($"F{Precision}")} kg";
+        }
+    }
+}
