0d5ddc0dd327abe2af309d911c1b6ddf656aae31
joey36 <71462893+joey364@users.noreply.github.com>
2023-06-22 15:22:12 +0000

    csharp(remote-control-competition): complete!

---
+++ csharp/remote-control-competition/RemoteControlCompetition.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+public interface IRemoteControlCar
+{
+    int DistanceTravelled { get; }
+    void Drive();
+}
+
+public class ProductionRemoteControlCar : IRemoteControlCar, IComparable<ProductionRemoteControlCar>
+{
+    public int DistanceTravelled { get; private set; }
+    public int NumberOfVictories { get; set; }
+
+    public int CompareTo(ProductionRemoteControlCar other)
+    {
+        if (NumberOfVictories < other.NumberOfVictories)
+            return -1;
+        else if (other.NumberOfVictories == NumberOfVictories)
+            return 0;
+        else
+            return 1;
+    }
+
+    public void Drive()
+    {
+        DistanceTravelled += 10;
+    }
+}
+
+public class ExperimentalRemoteControlCar : IRemoteControlCar
+{
+    public int DistanceTravelled { get; private set; }
+
+    public void Drive()
+    {
+        DistanceTravelled += 20;
+    }
+}
+
+public static class TestTrack
+{
+    public static void Race(IRemoteControlCar car)
+    {
+        car.Drive();
+    }
+
+    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
+        ProductionRemoteControlCar prc2)
+    {
+        var sortedList = new List<ProductionRemoteControlCar>() { prc1, prc2 };
+        sortedList.Sort();
+        return sortedList;
+    }
+}
