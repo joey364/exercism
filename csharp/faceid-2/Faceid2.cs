8072e475e3831db8d9b0b2c8fefad8e2db168db1
joey36 <71462893+joey364@users.noreply.github.com>
2023-10-27 09:02:15 +0000

    csharp(faceid-2): complete!

---
+++ csharp/faceid-2/Faceid2.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+
+public class FacialFeatures
+{
+    public string EyeColor { get; }
+    public decimal PhiltrumWidth { get; }
+
+    public FacialFeatures(string eyeColor, decimal philtrumWidth)
+    {
+        EyeColor = eyeColor;
+        PhiltrumWidth = philtrumWidth;
+    }
+
+    protected bool Equals(FacialFeatures other)
+    {
+        return EyeColor == other.EyeColor && PhiltrumWidth.Equals(other.PhiltrumWidth);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((FacialFeatures)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(EyeColor, PhiltrumWidth);
+    }
+}
+
+public class Identity
+{
+    public string Email { get; }
+    public FacialFeatures FacialFeatures { get; }
+
+    public Identity(string email, FacialFeatures facialFeatures)
+    {
+        Email = email;
+        FacialFeatures = facialFeatures;
+    }
+
+    protected bool Equals(Identity identity)
+    {
+        return identity.Email == Email && identity.FacialFeatures.Equals(FacialFeatures);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (this.GetType() != obj.GetType()) return false;
+        return Equals((Identity)obj);
+
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Email, FacialFeatures);
+    }
+}
+
+public class Authenticator
+{
+    private Identity _admin = new Identity("admin@exerc.ism", new FacialFeatures("green", 0.9m));
+    private HashSet<Identity> _identities = new();
+
+    public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB)
+    {
+        return faceA.Equals(faceB);
+    }
+
+    public bool IsAdmin(Identity identity)
+    {
+        return identity.Equals(_admin);
+    }
+
+    public bool Register(Identity identity)
+    {
+        return _identities.Add(identity);
+    }
+
+    public bool IsRegistered(Identity identity)
+    {
+        return _identities.Contains(identity);
+    }
+
+    public static bool AreSameObject(Identity identityA, Identity identityB)
+    {
+        return ReferenceEquals(identityA, identityB);
+    }
+}
