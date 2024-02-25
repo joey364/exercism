17eaf7c7c193c4e3a6cec403c2de1058cf1ed3b9
joey36 <71462893+joey364@users.noreply.github.com>
2024-02-25 10:46:56 +0000

    java(lasagna): complete!

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,17 @@
+public class Lasagna {
+  public int expectedMinutesInOven() {
+    return 40;
+  }
+
+  public int remainingMinutesInOven(int timeInOven) {
+    return this.expectedMinutesInOven() - timeInOven;
+  }
+
+  public int preparationTimeInMinutes(int numberOfLayers) {
+    return numberOfLayers * 2;
+  }
+
+  public int totalTimeInMinutes(int numberOfLayers, int minutesInOven) {
+    return this.preparationTimeInMinutes(numberOfLayers) + minutesInOven;
+  }
+}
